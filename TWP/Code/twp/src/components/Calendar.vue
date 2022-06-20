37059f419ce4b728c389d31bd2612ef4312c94c4
X-VINCENT <xavier.vincent@epitech.eu>
2022-06-20 23:09:55 +0200

    [ADD] /*TWP*/ Cards and Scrollbar

--- TWP/Code/twp/src/components/Calendar.vue
+++ TWP/Code/twp/src/components/Calendar.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="calendar">
-    <v-date-picker
-      is-dark
-      v-model="date"
-      :select-attribute="selectAttribute"
-      @dayclick="onDayClick"
-    />
+    <v-date-picker is-dark v-model="date" :select-attribute="selectAttribute" />
   </div>
 </template>
 
@@ -20,30 +15,8 @@ export default {
       },
     };
   },
-  computed: {
-    dates() {
-      return this.days.map((day) => day.date);
-    },
-    attributes() {
-      return this.dates.map((date) => ({
-        highlight: true,
-        dates: date,
-      }));
-    },
-  },
-  methods: {
-    onDayClick(day) {
-      const idx = this.days.findIndex((d) => d.id === day.id);
-      if (idx >= 0) {
-        this.days.splice(idx, 1);
-      } else {
-        this.days.push({
-          id: day.id,
-          date: day.date,
-        });
-      }
-    },
-  },
+  computed: {},
+  methods: {},
 };
 </script>
 
--- TWP/Code/twp/src/components/Home.vue
+++ TWP/Code/twp/src/components/Home.vue
@@ -1,13 +1,47 @@
 <template>
   <div class="home">
     <div class="list">
-      <h1>To do</h1>
+      <h1>Todo</h1>
+      <div class="card-box">
+        <div class="card">
+          <h1>Title</h1>
+          <p>Description</p>
+        </div>
+        <div class="card">
+          <h1>Title</h1>
+          <p>Description</p>
+        </div>
+        <div class="card">
+          <h1>Title</h1>
+          <p>Description</p>
+        </div>
+        <div class="card">
+          <h1>Title</h1>
+          <p>Description</p>
+        </div>
+        <div class="card">
+          <h1>Title</h1>
+          <p>Description</p>
+        </div>
+        <div class="card">
+          <h1>Title</h1>
+          <p>Description</p>
+        </div>
+      </div>
     </div>
     <div class="list">
       <h1>In progress</h1>
+      <div class="card">
+        <h1>Title</h1>
+        <p>Description</p>
+      </div>
     </div>
     <div class="list">
       <h1>Done</h1>
+      <div class="card">
+        <h1>Title</h1>
+        <p>Description</p>
+      </div>
     </div>
   </div>
 </template>
@@ -28,6 +62,7 @@ export default {
   flex-direction: column;
   align-items: center;
   margin: 24px auto;
+  padding: 0 10px;
   gap: 16px;
 
   position: relative;
@@ -39,7 +74,7 @@ export default {
   border-color: #4a5568;
   border-radius: 16px;
 
-  h1 {
+  & > h1 {
     width: 320px;
     height: 44px;
     font-family: BlinkMacSystemFont, -apple-system, Segoe UI, Roboto, Oxygen,
@@ -47,14 +82,85 @@ export default {
       sans-serif;
     font-style: normal;
     font-weight: 600;
-    font-size: 18px;
-    line-height: 28px;
+    font-size: 28px;
+    line-height: 200%;
 
     color: #fff;
+  }
+
+  .card-box {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 16px;
+    margin-bottom: 24px;
+    border-radius: 24px;
+
+    overflow-y: auto;
+    scrollbar-color: blue red;
+
+    &::-webkit-scrollbar {
+      width: 8px;
+      height: 100%;
+      background-color: #42b983;
+      border-radius: 3px;
+      padding-right: 20px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      background: #2c7d59;
+      border-radius: 3px;
+    }
+  }
+
+  .card {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 8px;
+    text-align: left;
+
+    width: 320px;
+
+    background: #d93535;
+
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.12),
+      0px 16px 32px rgba(0, 0, 0, 0.08);
+    border-radius: 8px;
 
     flex: none;
     order: 0;
     flex-grow: 0;
+
+    & > h1 {
+      width: 272px;
+      height: 24px;
+      margin: 10px 24px;
+
+      font-family: "SF Pro Text";
+      font-style: normal;
+      font-weight: 600;
+      font-size: 20px;
+      line-height: 150%;
+
+      color: #fff;
+    }
+
+    & > p {
+      width: 272px;
+      height: 40px;
+      margin: 10px 24px;
+
+      font-family: "SF Pro Text";
+      font-style: normal;
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 140%;
+
+      color: rgba(255, 255, 255, 0.75);
+    }
   }
 }
 </style>
