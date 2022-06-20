b492c02330c8c9c3ae40570d58285a5a8db454b6
X-VINCENT <xavier.vincent@epitech.eu>
2022-06-20 15:34:20 +0200

    [ADD] /*TWP*/ NavBar CSS

--- TWP/Code/twp/src/App.vue
+++ TWP/Code/twp/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/calendar">Calendar</router-link>
+    <ul class="nav-bar">
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/calendar">Calendar</router-link></li>
+    </ul>
   </nav>
   <router-view />
 </template>
@@ -18,8 +20,55 @@
 nav {
   padding: 30px;
 
+  .nav-bar {
+    border-radius: 25px;
+    height: fit-content;
+    display: inline-flex;
+    background-color: rgba(0, 0, 0, 0.4);
+    -webkit-backdrop-filter: blur(10px);
+    backdrop-filter: blur(10px);
+    align-items: center;
+    padding: 0 10px;
+
+    li {
+      list-style: none;
+      color: white;
+      font-family: sans-serif;
+      font-weight: bold;
+      padding: 12px 16px;
+      margin: 0 8px;
+      position: relative;
+      cursor: pointer;
+      white-space: nowrap;
+
+      &:before {
+        content: " ";
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 100%;
+        width: 100%;
+        z-index: -1;
+        transition: 0.2s;
+        border-radius: 25px;
+      }
+
+      &:hover {
+        color: black;
+
+        &:before {
+          background: linear-gradient(to bottom, #e8edec, #d2d1d3);
+          box-shadow: 0px 3px 20px 0px black;
+          transform: scale(1.2);
+        }
+      }
+    }
+  }
+
   a {
+    text-decoration: none;
     font-weight: bold;
+    font-size: 28px;
     color: #2c3e50;
 
     &.router-link-exact-active {
